2b9bd60e8332b57968d516e45872d5680e2ebef5
Knight <knight@localhost.localdomain>
2025-02-05 00:06:34 -0700

    Added bhp subdirectory with scripts

---
+++ bhp/tcp-client.py
@@ -0,0 +1,20 @@
+import socket
+
+# update to correct server
+target_host = "127.0.0.1"
+target_port = 9998
+
+# create socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect the client
+client.connect((target_host,target_port))
+
+# send some data
+client.send(b"Hello, Server!")
+
+# receive some data
+response = client.recv(4096)
+
+print(response.decode())
+client.close()
